e85852fb7e8a3b5660a5d94de85122db6a670056
steghy <steghy.github@proton.me>
2022-10-14 18:38:19 +0200

    update

--- src/juno/exception/DataException.java
+++ src/juno/exception/DataException.java
@@ -3,5 +3,8 @@ package juno.exception;
 public class DataException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	public DataException(String message) {
+		super(message);
+	}
 }
