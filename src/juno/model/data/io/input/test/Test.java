18b2eced986ca5d6e9cf8409cd6b4c3763d802e1
steghy <steghy.github@proton.me>
2022-11-23 00:36:51 +0100

    Update

--- src/juno/model/data/io/input/test/Test.java
+++ src/juno/model/data/io/input/test/Test.java
@@ -2,7 +2,15 @@ package juno.model.data.io.input.test;
 
 public class Test {
 
-    public static void main(Sting[] args) {
+    public static void main(String[] args) {
 
+        short var_1 = 4124;
+        long var_2 = 432423L;
+        double var_3 = 233123213;
+        float var_4 = 32132;
+
+        int var_int = (int) var_4;
+
+        System.out.println(var_int);
     }
 }
