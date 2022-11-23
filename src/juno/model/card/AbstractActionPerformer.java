7eec8ba782fe0426b6ce760d0e2f85941cfb4b2b
steghy <steghy.github@proton.me>
2022-11-23 19:36:20 +0100

    Update

--- src/juno/model/card/AbstractActionPerformer.java
+++ src/juno/model/card/AbstractActionPerformer.java
@@ -1,11 +1,36 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Simone Gentili
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package juno.model.card;
 
 import juno.model.deck.AbstractDeck;
 import juno.model.subjects.hands.InterfaceItemAdder;
-import juno.model.subjects.shift.InterfaceInverter;
 import juno.model.subjects.players.InterfaceSubjectsManager;
-import juno.model.subjects.shift.InterfaceTurnMover;
+import juno.model.subjects.shift.InterfaceInverter;
 import juno.model.subjects.shift.InterfaceTurnJumper;
+import juno.model.subjects.shift.InterfaceTurnMover;
 
 public abstract class AbstractActionPerformer<T, E, C> implements InterfaceActionPerformer<E, C> {
 
--- src/juno/view/pages/main/menu/MenuPanelConfigurator.java
+++ src/juno/view/pages/main/menu/MenuPanelConfigurator.java
@@ -1,3 +1,28 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Simone Gentili
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package juno.view.pages.main.menu;
 
 import juno.controller.ChangePanelAction;
@@ -6,7 +31,8 @@ import juno.model.data.io.output.ExitManager;
 import juno.view.factories.ButtonFactory;
 import juno.view.factories.ButtonLibrary;
 import juno.view.pages.main.card.MainCardPanel;
-import juno.view.util.*;
+import juno.view.util.ImageResizer;
+import juno.view.util.RoundedBorder;
 
 import javax.swing.*;
 import javax.swing.border.Border;
